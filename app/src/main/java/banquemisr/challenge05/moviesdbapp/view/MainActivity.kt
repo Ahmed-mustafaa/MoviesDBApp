79a038705f3b3c750075335d89668b899a8c1fe0
Ahmed-mustafaa <medooortega.mx@gmail.com>
2024-10-21 20:54:30 +0300

    MovieDetailsScreen

--- app/src/main/java/banquemisr/challenge05/moviesdbapp/view/MainActivity.kt
+++ app/src/main/java/banquemisr/challenge05/moviesdbapp/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package banquemisr.challenge05.moviesdbapp
+package banquemisr.challenge05.moviesdbapp.view
 
 import android.os.Build
 import android.os.Bundle
@@ -9,11 +9,7 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.rememberNavController
 import banquemisr.challenge05.moviesdbapp.Model.Database.DatabaseBuilder
 import banquemisr.challenge05.moviesdbapp.Model.MovieRepository.MovieRepository
@@ -21,7 +17,6 @@ import banquemisr.challenge05.moviesdbapp.Service.RetrofitClient
 import banquemisr.challenge05.moviesdbapp.ViewModel.MoviesViewModel
 import banquemisr.challenge05.moviesdbapp.ViewModel.MoviesViewModelFactory
 import banquemisr.challenge05.moviesdbapp.ui.theme.MoviesDBAppTheme
-import banquemisr.challenge05.moviesdbapp.view.MainScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -32,44 +27,25 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             MoviesDBAppTheme {
-                val navController = rememberNavController() // Initialize NavController
+                val navController = rememberNavController() // Create navController here
+
+                // Initialize NavController
                 val movieDao = DatabaseBuilder.getInstance(applicationContext).movieDao()
                 val repository = MovieRepository(RetrofitClient.apiService, movieDao)
+/*
                 val factory = MoviesViewModelFactory(repository)
-
-
-
-
-                // Create ViewModel using the factory
+*/
                 val viewModel: MoviesViewModel = MoviesViewModelFactory(repository).create(MoviesViewModel::class.java)
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        navController = navController,
-                        viewModel = viewModel,
-                        modifier = Modifier.padding(innerPadding)
-                    ) // Add MainScreen
+                    Nav(navController = navController, viewModel = viewModel, padding = innerPadding)
 
                 }
 
             }
 
         }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesDBAppTheme {
-        Greeting("Android")
-    }
-}
