9f99acb435632b35f5cd1a0e1be7d70aaf984928
Ahmed-mustafaa <medooortega.mx@gmail.com>
2024-10-21 17:30:32 +0300

    Created (MovieEntity) to fetch movieDetails using movie ID at the MovieDetailsScreen

--- app/src/main/java/banquemisr/challenge05/moviesdbapp/Model/Database/MovieEntity.kt
+++ app/src/main/java/banquemisr/challenge05/moviesdbapp/Model/Database/MovieEntity.kt
@@ -2,13 +2,22 @@ package banquemisr.challenge05.moviesdbapp.Model.Database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import banquemisr.challenge05.moviesdbapp.Model.Genre
+
 @Entity(tableName = "movieEntity")
 data class MovieEntity(
-    @PrimaryKey val id: Int,            // Movie ID from TMDb
-    val title: String,                  // Movie title
-    val releaseDate: String,            // Release date of the movie
-    val posterPath: String?,            // Path to the movie's poster image
-    val overview: String,               // Movie overview/description
-    val voteAverage1: String,
-    val voteAverage: Double
+    val adult: Boolean,
+    val backdrop_path: String?,
+    val genres: List<Genre>, // You need genres specifically for details
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String?,
+    val popularity: Double,
+    val poster_path: String?,
+    val release_date: String,
+    val runtime: Int?,
+    val title: String,
+    val vote_average: Double,
+    val vote_count: Int
 )
