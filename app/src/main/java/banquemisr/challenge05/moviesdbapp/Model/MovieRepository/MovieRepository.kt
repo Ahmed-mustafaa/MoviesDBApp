025df9cbd555f83e4238ac24d80c02ac67eefef6
Ahmed-mustafaa <medooortega.mx@gmail.com>
2024-10-21 17:44:30 +0300

    Adjusting MovieRepository to fetch details using ID

--- app/src/main/java/banquemisr/challenge05/moviesdbapp/Model/MovieRepository/MovieRepository.kt
+++ app/src/main/java/banquemisr/challenge05/moviesdbapp/Model/MovieRepository/MovieRepository.kt
@@ -1,59 +1,72 @@
+
 package banquemisr.challenge05.moviesdbapp.Model.MovieRepository
 
+import banquemisr.challenge05.moviesdbapp.Model.Database.Movie
 import android.net.http.HttpException
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresExtension
+
 import banquemisr.challenge05.moviesdbapp.Model.Database.MovieDao
 import banquemisr.challenge05.moviesdbapp.Model.Database.MovieEntity
-import banquemisr.challenge05.moviesdbapp.Model.Movie
-import banquemisr.challenge05.moviesdbapp.Service.TmdbApiService
+
+import banquemisr.challenge05.moviesdbapp.Service.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.withContext
 
-class MovieRepository (private  val apiService: TmdbApiService,
+
+class MovieRepository (private  val apiService: ApiService,
                        private val movieDao: MovieDao
 ) {
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     suspend fun getNowPlayingMovies(apikey: String): Flow<List<Movie>> = flow {
-        try{
+        try {
             val response = apiService.getNowPlayingMovies(apikey)
             val movies = response.results.map {
                 Movie(
-                    it.adult,
-                    it.backdrop_path,
-                    it.id,
-                    it.original_language,
-                    it.original_title,
-                    it.overview,
-                    it.popularity,
-                    it.poster_path,
-                    it.release_date,
-                    it.title,
-                    it.video,
-                    it.vote_average,
-                    it.vote_count
+                    adult = it.adult,
+                    backdrop_path = it.backdrop_path,
+                    id = it.id,
+                    budget = it.budget,
 
+                    original_language = it.original_language,
+                    original_title = it.original_title,
+                    overview = it.overview,
+                    popularity = it.popularity,
+                    poster_path = it.poster_path,
+
+                    release_date = it.release_date,
+                    revenue = it.revenue,
+                    runtime = it.runtime,
+
+                    title = it.title,
+                    video = it.video,
+                    vote_average = it.vote_average,
+                    vote_count = it.vote_count
                 )
             }
+
+
             movieDao.insertMovies(movies)
             emit(movies)
 
-        }catch (e: HttpException){
+        } catch (e: HttpException) {
             val cachedMovies = movieDao.getAllMovies().first()  // Collect the Flow into a List
-            emit(cachedMovies)        }
+            emit(cachedMovies)
+        }
 
 
-        }.flowOn(Dispatchers.IO)
+    }.flowOn(Dispatchers.IO)
 
-    suspend fun insertMovies(movies: List<Movie>){
+
+    suspend fun insertMovies(movies: List<Movie>) {
         movieDao.insertMovies(movies)
     }
-    suspend fun insertMovie(movie: Movie){
+
+    suspend fun insertMovie(movie: Movie) {
         movieDao.insertMovie(movie)
     }
 
@@ -64,55 +77,24 @@ class MovieRepository (private  val apiService: TmdbApiService,
             Log.d("PopularMoviesFromMovieRepository", "Response: $response")
             val movies = response.results.map {
                 Movie(
-                    it.adult,
-                    it.backdrop_path,
-                    it.id,
-                    it.original_language,
-                    it.original_title,
-                    it.overview,
-                    it.popularity,
-                    it.poster_path,
-                    it.release_date,
-                    it.title,
-                    it.video,
-                    it.vote_average,
-                    it.vote_count
+                    adult = it.adult,
+                    backdrop_path = it.backdrop_path,
+                    id = it.id,
+                    budget = it.budget,
+                     // Convert origin countries to string
+                    original_language = it.original_language,
+                    original_title = it.original_title,
+                    overview = it.overview,
+                    popularity = it.popularity,
+                    poster_path = it.poster_path,
+                    release_date = it.release_date,
+                    revenue = it.revenue,
+                    runtime = it.runtime,
 
-
-                )
-            }
-
-                movieDao.insertMovies(movies)
-                emit(movies)
-
-
-        }catch (e: HttpException){
-            val cachedMovies = movieDao.getAllMovies().first()
-            emit(cachedMovies)
-        }
-    }.flowOn(Dispatchers.IO)
-    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    suspend fun getUpComingMovies(apikey: String): Flow<List<Movie>> = flow {
-        try {
-            val response = apiService.getPopularMovies(apikey)
-            Log.d("PopularMoviesFromMovieRepository", "Response: $response")
-            val movies = response.results.map {
-                Movie(
-                    it.adult,
-                    it.backdrop_path,
-                    it.id,
-                    it.original_language,
-                    it.original_title,
-                    it.overview,
-                    it.popularity,
-                    it.poster_path,
-                    it.release_date,
-                    it.title,
-                    it.video,
-                    it.vote_average,
-                    it.vote_count
-
-
+                    title = it.title,
+                    video = it.video,
+                    vote_average = it.vote_average,
+                    vote_count = it.vote_count
                 )
             }
 
@@ -120,9 +102,62 @@ class MovieRepository (private  val apiService: TmdbApiService,
             emit(movies)
 
 
-        }catch (e: HttpException){
+        } catch (e: HttpException) {
             val cachedMovies = movieDao.getAllMovies().first()
             emit(cachedMovies)
         }
     }.flowOn(Dispatchers.IO)
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    suspend fun getUpComingMovies(apikey: String): Flow<List<Movie>> = flow {
+        try {
+            val response = apiService.getUpcomingMovies(apikey)
+            Log.d("PopularMoviesFromMovieRepository", "Response: $response")
+            val movies = response.results.map {
+                Movie(
+                    adult = it.adult,
+                    backdrop_path = it.backdrop_path,
+                    id = it.id,
+                    budget = it.budget,
+                    // Convert origin countries to string
+                    original_language = it.original_language,
+                    original_title = it.original_title,
+                    overview = it.overview,
+                    popularity = it.popularity,
+                    poster_path = it.poster_path,
+                    release_date = it.release_date,
+                    revenue = it.revenue,
+                    runtime = it.runtime,
+                    title = it.title,
+                    video = it.video,
+                    vote_average = it.vote_average,
+                    vote_count = it.vote_count
+                )
+            }
+
+
+
+            movieDao.insertMovies(movies)
+            emit(movies)
+
+
+        } catch (e: HttpException) {
+            val cachedMovies = movieDao.getAllMovies().first()
+            emit(cachedMovies)
+        }
+    }.flowOn(Dispatchers.IO)
+
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+suspend fun getMovieDetailsByID(movieID: Int?, apikey: String): Flow<MovieEntity> = flow {
+    try{
+    val response = apiService.getMovieDetails(movieID, apikey)
+    emit(response)
 }
+
+    catch (e: HttpException) {
+        // Handle exceptions or cache the details if necessary
+    }
+}.flowOn(Dispatchers.IO)
+}
+
